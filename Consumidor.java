da7eee624eeb9ac43b7d68fb6c222f249cc65bc2
Leonardo Britto <leobritto160@gmail.com>
2023-01-30 22:45:15 -0300

    Classe Consumidor

---
+++ Consumidor.java
@@ -0,0 +1,17 @@
+package Entites;
+public class Consumidor extends Thread {
+
+    private Numero numero;
+
+    public Consumidor(Numero numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public void run() {
+
+        this.numero.consumir(this.getName());
+
+    }
+
+}
