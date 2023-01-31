17d2ae131eef74c8ef5c8c32fb42484d61ca8786
Leonardo Britto <leobritto160@gmail.com>
2023-01-30 22:49:56 -0300

    Classe Produtor

---
+++ Produtor.java
@@ -0,0 +1,21 @@
+package Entites;
+public class Produtor extends Thread {
+
+	private Numero numero;
+
+	public Produtor(Numero numero) {
+		this.numero = numero;
+	}
+	
+	// O metodo Run sobreposto da classe Thread, ele roda quando o metodo start() chama
+
+	@Override
+	public void run() {
+		
+		// atributo Numero executa o metodo produzir passando como parametro o nome thread (Thread - 0)
+
+		this.numero.produzir(this.getName());
+
+	}
+
+}
